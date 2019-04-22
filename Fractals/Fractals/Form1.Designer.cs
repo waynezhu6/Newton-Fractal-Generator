83bbe843defbeecc3dc15dbffa6acc8e0cad84b5
Wayne Zhu <48568003+waynezhu6@users.noreply.github.com>
2019-04-22 13:38:43 -0400

    Add files via upload

---
+++ Fractals/Fractals/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBox = new System.Windows.Forms.PictureBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtIteration = new System.Windows.Forms.TextBox();
+            this.lblIterations = new System.Windows.Forms.Label();
+            this.lblEquation = new System.Windows.Forms.Label();
+            this.txtEquation = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.lblViewport = new System.Windows.Forms.Label();
+            this.txtViewport = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBox
+            // 
+            this.pBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pBox.Location = new System.Drawing.Point(13, 13);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(1000, 1000);
+            this.pBox.TabIndex = 0;
+            this.pBox.TabStop = false;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(1022, 128);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(238, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Render";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtIteration
+            // 
+            this.txtIteration.Location = new System.Drawing.Point(1089, 13);
+            this.txtIteration.Name = "txtIteration";
+            this.txtIteration.Size = new System.Drawing.Size(171, 20);
+            this.txtIteration.TabIndex = 2;
+            // 
+            // lblIterations
+            // 
+            this.lblIterations.AutoSize = true;
+            this.lblIterations.Location = new System.Drawing.Point(1019, 16);
+            this.lblIterations.Name = "lblIterations";
+            this.lblIterations.Size = new System.Drawing.Size(50, 13);
+            this.lblIterations.TabIndex = 3;
+            this.lblIterations.Text = "Iterations";
+            // 
+            // lblEquation
+            // 
+            this.lblEquation.AutoSize = true;
+            this.lblEquation.Location = new System.Drawing.Point(1019, 42);
+            this.lblEquation.Name = "lblEquation";
+            this.lblEquation.Size = new System.Drawing.Size(49, 13);
+            this.lblEquation.TabIndex = 5;
+            this.lblEquation.Text = "Equation";
+            // 
+            // txtEquation
+            // 
+            this.txtEquation.Location = new System.Drawing.Point(1089, 39);
+            this.txtEquation.Name = "txtEquation";
+            this.txtEquation.Size = new System.Drawing.Size(171, 20);
+            this.txtEquation.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(1022, 94);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(90, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Color By Root";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(1118, 94);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(110, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "Color By Iterations";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // lblViewport
+            // 
+            this.lblViewport.AutoSize = true;
+            this.lblViewport.Location = new System.Drawing.Point(1019, 68);
+            this.lblViewport.Name = "lblViewport";
+            this.lblViewport.Size = new System.Drawing.Size(48, 13);
+            this.lblViewport.TabIndex = 10;
+            this.lblViewport.Text = "Viewport";
+            // 
+            // txtViewport
+            // 
+            this.txtViewport.Location = new System.Drawing.Point(1089, 65);
+            this.txtViewport.Name = "txtViewport";
+            this.txtViewport.Size = new System.Drawing.Size(171, 20);
+            this.txtViewport.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1272, 1026);
+            this.Controls.Add(this.lblViewport);
+            this.Controls.Add(this.txtViewport);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.lblEquation);
+            this.Controls.Add(this.txtEquation);
+            this.Controls.Add(this.lblIterations);
+            this.Controls.Add(this.txtIteration);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.pBox);
+            this.Name = "Form1";
+            this.Text = "Fractals";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBox;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtIteration;
+        private System.Windows.Forms.Label lblIterations;
+        private System.Windows.Forms.Label lblEquation;
+        private System.Windows.Forms.TextBox txtEquation;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label lblViewport;
+        private System.Windows.Forms.TextBox txtViewport;
+    }
+}
+
---
+++ Fractals/Fractals/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using MathNet.Numerics;
+using System.Threading;
+
+namespace Fractals
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public double[] coefficients;
+        public bool colorRoots = true;
+        public int iterations = -1;
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            float tolerance = 0.001f;
+            iterations = Convert.ToInt32(txtIteration.Text);
+            pBox.Image = new Bitmap(pBox.Width, pBox.Height);
+
+            string[] viewport = txtViewport.Text.Split(',');
+            double viewportX = Convert.ToDouble(viewport[0]);
+            double viewportY = Convert.ToDouble(viewport[1]);
+
+            string[] split = txtEquation.Text.Split(',');
+
+            coefficients = new double[split.Length];
+            for(int i = 0; i < split.Length; i++)
+            {
+                coefficients[coefficients.Length - 1 - i] = Convert.ToDouble(split[i]);
+            }
+
+            Complex[] roots = GetRoots(coefficients);
+
+            for (float y = 0; y < pBox.Height; y = y + 1f)
+            {
+                for (float x = 0; x < pBox.Width; x = x + 1f)
+                {
+                    Complex z = new Complex((x - 500f)/500f * viewportX, (y - 500f)/500f * viewportY);
+                    Color color = NewtonRaphson(z, iterations, tolerance, roots);
+                    ((Bitmap)pBox.Image).SetPixel(Convert.ToInt32(x), Convert.ToInt32(y), color);
+                }
+            }
+        }
+
+        public Color NewtonRaphson(Complex x, int i, float tolerance, Complex[] roots)
+        {
+
+            Color[] colors = {
+                    Color.Red,
+                    Color.Green,
+                    Color.Blue,
+                    Color.Yellow,
+                    Color.Purple,
+                    Color.Orange,
+                    Color.GreenYellow,
+                    Color.OrangeRed,
+                    Color.BlueViolet
+            };
+
+            if (i != 0)
+            {
+                Complex temp = Complex.Divide(F(x), FDerivative(x));
+                x = Complex.Subtract(x, temp);
+
+                for(int j = 0; j < roots.Length; j++)
+                {
+                    Complex difference = Complex.Subtract(x, roots[j]);
+                    if (Math.Abs(difference.Real) < tolerance && Math.Abs(difference.Imaginary) < tolerance)
+                    {
+                        if(colorRoots)
+                        {
+                            int value = Convert.ToInt32(Convert.ToDouble(i) / Convert.ToDouble(iterations) * 255);
+                            Color color = Color.FromArgb(value, colors[j].R, colors[j].B, colors[j].G);
+                            return color;
+                        }
+                        else
+                        {
+                            int value = Convert.ToInt32(Convert.ToDouble(i) / Convert.ToDouble(iterations) * 255);
+                            Color color = Color.FromArgb(value, value, value);
+                            return color;
+                        }
+                        
+                    }
+                }
+                return NewtonRaphson(x, i - 1, tolerance, roots);
+            }
+            else
+            {
+                return Color.Black;
+            }
+
+        }
+
+        public Complex F(Complex x)
+        {
+            Complex result = new Complex(0, 0);
+            for(int i = 0; i < coefficients.Length; i++)
+            {
+                Complex tempCoefficient = new Complex(coefficients[coefficients.Length - 1 - i], 0);
+                Complex tempVariable = Complex.Pow(x, coefficients.Length - 1 - i);
+                Complex tempResult = Complex.Multiply(tempCoefficient, tempVariable);
+                result = Complex.Add(result, tempResult);
+            }
+            return result;
+        }
+
+        public Complex FDerivative(Complex x)
+        {
+            Complex result = new Complex(0, 0);
+            for(int i = 1; i < coefficients.Length; i++)
+            {
+                if(coefficients[i] != 0)
+                {
+                    result = result + coefficients[i] * i * Complex.Pow(x, i - 1);
+                }
+            }
+
+            return result;
+            //return Complex.Multiply(8, Complex.Pow(x, 7)) + Complex.Multiply(60, Complex.Pow(x, 3));
+        }
+
+        public Complex[] GetRoots(Double[] coefficients)
+        {
+            return FindRoots.Polynomial(coefficients);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            colorRoots = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            colorRoots = true;
+        }
+    }
+}
+
+/*
+ *             string[] polynomials = txtEquation.Text.Split('+');
+            for(int i = 0; i < polynomials.Length; i++)
+            {
+                if(polynomials[i].Contains('-'))
+                {
+                    Array.Resize(ref polynomials, polynomials.Length + 1);
+                    for(int j = i + 1; j < polynomials.Length - 1; j++)
+                    {
+                        polynomials[j + 1] = polynomials[j];
+                    }
+                    string[] tempArray = polynomials[i].Split('-');
+                    polynomials[i] = tempArray[0];
+                    polynomials[i + 1] = tempArray[1];
+                }
+            }
+
+            string[] tempSplit = polynomials[0].Split('^');
+            int tempLength = Convert.ToInt32(tempSplit[1]);
+            double[] coefficients = new double[tempLength];
+
+            for(int i = 0; i < polynomials.Length; i++)
+            {
+                string[] temp = polynomials[i].Split('^');
+                if(temp.Length > 1)
+                {
+                    if(temp[0] == "x")
+                    {
+                        coefficients[Convert.ToInt32(temp[1]) - 1] = Convert.ToDouble(temp[0]);
+                    }
+                    coefficients[Convert.ToInt32(temp[1]) - 1] = Convert.ToDouble(temp[0]);
+                }
+                else
+                {
+                    coefficients[0] = Convert.ToDouble(temp[0]);
+                }
+            }
+
+            Complex[] roots = GetRoots(coefficients);*/
+
+/*            /*int iteration = 0;
+        while(iteration < i)
+        {
+            Complex temp = Complex.Divide(F(x), FDerivative(x));
+            x = Complex.Subtract(x, temp);
+
+            for(int j = 0; j < roots.Length; j++)
+            {
+                Complex difference = Complex.Subtract(x, roots[j]);
+                if (Math.Abs(difference.Real) < tolerance && Math.Abs(difference.Imaginary) < tolerance)
+                {
+                    if(colorRoots)
+                    {
+                        return colors[j];
+                    }
+                    else
+                    {
+                        int value = Convert.ToInt32(Convert.ToDouble(iteration) / Convert.ToDouble(i) * 255);
+                        Color color = Color.FromArgb(value, value, value);
+                        return color;
+                    }
+
+                }
+            }
+            iteration++;
+        }
+        return Color.Black;*/
